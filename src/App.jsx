f5272730beed83c30127ef7e40fa5ebc2a1ff790
Kiet Phan <kietphan54k2@gmail.com>
2025-12-01 20:22:07 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -269,13 +269,24 @@ function App() {
               </div>
               <div className="time-input">
                 <label>Seconds</label>
-                <input
-                  type="number"
-                  min="0"
-                  max="59"
+                <select
                   value={seconds}
-                  onChange={(e) => setSeconds(Math.max(0, parseInt(e.target.value) || 0))}
-                />
+                  onChange={(e) => setSeconds(parseInt(e.target.value))}
+                  className="time-select"
+                >
+                  <option value="0">0</option>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                  <option value="25">25</option>
+                  <option value="30">30</option>
+                  <option value="35">35</option>
+                  <option value="40">40</option>
+                  <option value="45">45</option>
+                  <option value="50">50</option>
+                  <option value="55">55</option>
+                </select>
               </div>
             </div>
           </div>
